d7356a5100ba4ab2546c73e845ca4782ba538054
Joaquin <joaquinlucascortez@gmail.com>
2024-12-23 09:29:25 -0300

    [Feature] Implement API endpoint to fetch images from Unsplash (Finished)

--- src/unsplash/unsplash.service.ts
+++ src/unsplash/unsplash.service.ts
@@ -1,22 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 @Injectable()
 export class UnsplashService {
     private readonly UNSPLASH_URL = 'https://api.unsplash.com/search/photos';
+    private readonly UNSPLASH_ACCESS_KEY = process.env.UNSPLASH_ACCESS_KEY;
 
-    async fetchPhotos(spot: string): Promise<{ imageUrl: string }> {
-        try {
-            const response = await axios.get(this.UNSPLASH_URL, {
-                params: { query: spot, per_page: 1 },
-                headers: { Authorization: `CLIENT-ID ${process.env.UNSPLASH_ACCESS_KEY}` },
-            });
+    async fetchPhotos(spot: string): Promise<{ imageUrl: string; author: {firstName: string; lastName: string} }> {
+        const enhancedUrl = `${spot} beach`
+        const url = `${this.UNSPLASH_URL}?query=${encodeURIComponent(enhancedUrl)}&client_id=${this.UNSPLASH_ACCESS_KEY}`
+        const response = await axios.get(url);
 
-            const imageUrl = response.data.results[0]?.urls?.regular || '';
-            return { imageUrl };
-        } catch (error) {
-            console.error('Error fetching data from Unsplash.', error);
-            throw new Error('Unable to fetch image.');
-        }
+        if(!response || !response.data.results.length) {
+            return {
+                imageUrl: 'https://via.placeholder.com/800x400?text=No+Image+Available',
+                author: { firstName: 'N/A', lastName: 'N/A' }
+            }
+        };
+        
+        const result = response.data.results[0];
+        const imageUrl = result?.urls?.regular || 'https://via.placeholder.com/800x400?text=No+Image+Available';
+        const author = {
+            firstName: result?.user?.first_name || 'Unknown',
+            lastName: result?.user?.last_name || '',
+        };
+        
+        return { imageUrl, author };
+        
     }
 }
