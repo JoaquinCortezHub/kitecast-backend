a3c9f0e8639a64bd0cf87645462e5c25b82a32e3
Joaquin <joaquinlucascortez@gmail.com>
2025-01-22 17:28:42 -0300

    [Clean-up] Delete unused imports .

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { WeatherModule } from './weather/weather.module';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma.service';
 import { UnsplashModule } from './unsplash/unsplash.module';
 import { ForecastModule } from './forecast/forecast.module';
-import { MapsModule } from './maps/maps.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true
-  }), WeatherModule, UserModule, AuthModule, UnsplashModule, ForecastModule, MapsModule],
+  }), WeatherModule, UnsplashModule, ForecastModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
