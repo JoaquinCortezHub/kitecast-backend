29ead2fb1e88422d04a41325162ad6ca87d5a338
Joaquin <joaquinlucascortez@gmail.com>
2025-02-02 19:55:45 -0300

    [Update] Update CORS origin allowed domains.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { config } from "dotenv";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+          'http://localhost:3000',
+          'https://kitespot.netlify.app'
+        ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true
   })
